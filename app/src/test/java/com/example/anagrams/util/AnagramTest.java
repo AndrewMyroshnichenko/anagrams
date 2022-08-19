4281a49f7d92fc62781cc221d7abbb89e45d7aac
Andrew Miroshnichenko <mobilemiron@gmail.com>
2022-08-19 14:02:49 +0300

    feat: added Unit test for Anagram class

---
+++ app/src/test/java/com/example/anagrams/util/AnagramTest.java
@@ -0,0 +1,14 @@
+package com.example.anagrams.util;
+
+import com.example.anagrams.utils.Anagram;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AnagramTest {
+
+    @Test
+    public void testAnagramIsWorks(){
+        Assert.assertEquals("ol1leH dlroW!", Anagram.anagram("He1llo World!"));
+    }
+}
