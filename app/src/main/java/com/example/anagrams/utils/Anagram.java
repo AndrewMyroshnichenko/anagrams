ae049ce5a9533e038c6dbb665135655ffd8bc5ef
Andrew Miroshnichenko <mobilemiron@gmail.com>
2022-08-20 01:43:04 +0300

    feat: added new method to Anagram class reverseExceptXL test: Added tests from task Foxmided and for new added method

--- app/src/main/java/com/example/anagrams/utils/Anagram.java
+++ app/src/main/java/com/example/anagrams/utils/Anagram.java
@@ -7,27 +7,59 @@ public class Anagram {
     public static String reverseOnlyLetters(String inputString){
 
         if(inputString == null) {
-            inputString = "esaelP, etirw a esarhp!";
+            inputString = "Please, write a phrase!";
+        } else {
+            String [] inputStringToArray = inputString.split(" ");
+
+            for(int i = 0; i < inputStringToArray.length; i++){
+                char [] inputWord = inputStringToArray[i].toCharArray();
+                char [] tempArrayForLetters = fillTempArrayWithLetters(inputWord);
+                char [] outputWord = new char[inputWord.length];
+                Arrays.fill(outputWord, (char) '\u0000');
+                outputWord = fillNonLetters(inputWord);
+                outputWord = finalNonLettersReversFilling(tempArrayForLetters, outputWord);
+                inputStringToArray[i] = new String(outputWord);
+            }
+
+            StringBuilder reversedString = new StringBuilder();
+            for (String s : inputStringToArray) {
+                reversedString.append(s).append(" ");
+            }
+
+            return  reversedString.toString().trim();
+
+        }
+        return inputString;
+
+
+    }
+
+    public static String reverseExceptXL(String inputString){
+        if(inputString == null) {
+            inputString = "Please, write a phrase!";
+        } else {
+            String [] inputStringToArray = inputString.split(" ");
+
+            for(int i = 0; i < inputStringToArray.length; i++){
+                char [] inputWord = inputStringToArray[i].toCharArray();
+                char [] tempArrayWithoutXL = fillTempArrayWithoutXL(inputWord);
+                char [] outputWord = new char[inputWord.length];
+                Arrays.fill(outputWord, (char) '\u0000');
+                outputWord = fillArrayWithXL(inputWord);
+                outputWord = finalNonXLLettersReversFilling(tempArrayWithoutXL, outputWord);
+                inputStringToArray[i] = new String(outputWord);
+            }
+
+            StringBuilder reversedString = new StringBuilder();
+            for (String s : inputStringToArray) {
+                reversedString.append(s).append(" ");
+            }
+
+            return  reversedString.toString().trim();
+
         }
 
-        String [] inputStringToArray = inputString.split(" ");
-
-        for(int i = 0; i < inputStringToArray.length; i++){
-            char [] inputWord = inputStringToArray[i].toCharArray();
-            char [] tempArrayForLetters = fillTempArrayWithLetters(inputWord);
-            char [] outputWord = new char[inputWord.length];
-            Arrays.fill(outputWord, (char) '\u0000');
-            outputWord = fillNonLetters(inputWord);
-            outputWord = fillLetters(tempArrayForLetters, outputWord);
-            inputStringToArray[i] = new String(outputWord);
-        }
-
-        StringBuilder ReversedString = new StringBuilder();
-        for (String s : inputStringToArray) {
-            ReversedString.append(s).append(" ");
-        }
-
-        return  ReversedString.toString().trim();
+        return inputString;
     }
 
     private static char [] fillNonLetters(char [] inputArray){
@@ -54,23 +86,68 @@ public class Anagram {
         return lettersArray;
     }
 
-    private static char [] fillLetters(char [] lettersArray, char [] outputArray){
+    private static char [] finalNonLettersReversFilling(char [] lettersArray, char [] outputArray){
         for (int j = 0; j < outputArray.length; j++) {
-            if(Character.isLetter(lettersArray[j])) {
+            if(lettersArray[j] != '\u0000') {
                 int counter = 0;
-                boolean isSeted = false;
-                while (!isSeted){
+                boolean wholeArrayIsSet = false;
+                while (!wholeArrayIsSet){
                     if (outputArray[outputArray.length - 1 - counter] == '\u0000'){
                         outputArray[outputArray.length - 1 - counter] = lettersArray[j];
-                        isSeted = true;
+                        wholeArrayIsSet = true;
                     } else {
                         counter++;
                     }
+
+                }
+            }
+        }
+
+        return outputArray;
+
+    }
+
+    private static char [] fillTempArrayWithoutXL(char [] inputArray){
+        char [] withoutXLArray = new char[inputArray.length];
+        for (int j = 0; j < inputArray.length; j++) {
+            if(inputArray[j] == 'x' || inputArray[j] == 'X' || inputArray[j] == 'l' || inputArray[j] == 'L'){
+                withoutXLArray[j] = '\u0000';
+            } else {
+                withoutXLArray[j] = inputArray[j];
+            }
+        }
+        return withoutXLArray;
+    }
+
+    private static char [] fillArrayWithXL(char [] inputArray){
+        char [] outputArray = new char[inputArray.length];
+        for (int j = 0; j < inputArray.length; j++){
+            if(inputArray[j] == 'x' || inputArray[j] == 'X' || inputArray[j] == 'l' || inputArray[j] == 'L'){
+                outputArray[j] = inputArray[j];
+            } else {
+                outputArray[j] = '\u0000';
+            }
+        }
+        return outputArray;
+    }
+
+    private static char [] finalNonXLLettersReversFilling(char [] nonXLLettersArray, char [] outputArray){
+        for (int j = 0; j < outputArray.length; j++) {
+            if(nonXLLettersArray[j] != '\u0000') {
+                int counter = 0;
+                boolean wholeArrayIsSet = false;
+                while (!wholeArrayIsSet){
+                    if (outputArray[outputArray.length - 1 - counter] == '\u0000'){
+                        outputArray[outputArray.length - 1 - counter] = nonXLLettersArray[j];
+                        wholeArrayIsSet = true;
+                    } else {
+                        counter++;
+                    }
+
                 }
             }
         }
 
         return outputArray;
     }
-
 }
--- app/src/test/java/com/example/anagrams/util/AnagramTest.java
+++ app/src/test/java/com/example/anagrams/util/AnagramTest.java
@@ -8,12 +8,52 @@ import org.junit.Test;
 public class AnagramTest {
 
     @Test
-    public void testAnagramIsWorks(){
+    public void testReverseOnlyLettersIsWorks(){
         Assert.assertEquals("ol1leH dlroW!", Anagram.reverseOnlyLetters("He1llo World!"));
     }
 
     @Test
-    public void testAngagramWhenNull(){
+    public void testReverseOnlyLettersWhenNull(){
         Assert.assertEquals("Please, write a phrase!", Anagram.reverseOnlyLetters(null));
+    }
+
+    @Test
+    public void testReverseOnlyLettersFoxmindedInput1(){
+        Assert.assertEquals("dednimxoF looc 24/7", Anagram.reverseOnlyLetters("Foxminded cool 24/7"));
+    }
+
+    @Test
+    public void testReverseOnlyLettersFoxmindedInput2(){
+        Assert.assertEquals("dcba hgfe", Anagram.reverseOnlyLetters("abcd efgh"));
+    }
+
+    @Test
+    public void testReverseOnlyLettersFoxmindedInput3(){
+        Assert.assertEquals("d1cba hgf!e", Anagram.reverseOnlyLetters("a1bcd efg!h"));
+    }
+
+    @Test
+    public void testReverseExceptXLisWorks(){
+        Assert.assertEquals("I raew XL trihs-t", Anagram.reverseExceptXL("I wear XL t-shirt"));
+    }
+
+    @Test
+    public void testReverseExceptXLWhenNull(){
+        Assert.assertEquals("Please, write a phrase!", Anagram.reverseExceptXL(null));
+    }
+
+    @Test
+    public void testReverseExceptXLFoxmindedInput1(){
+        Assert.assertEquals("dexdnimoF oocl 7/42", Anagram.reverseExceptXL("Foxminded cool 24/7"));
+    }
+
+    @Test
+    public void testReverseExceptXLFoxmindedInput2(){
+        Assert.assertEquals("dcba hgfe", Anagram.reverseExceptXL("abcd efgh"));
+    }
+
+    @Test
+    public void testReverseExceptXLFoxmindedInput3(){
+        Assert.assertEquals("a1bcd efglh", Anagram.reverseExceptXL("dcb1a hgfle"));
     }
 }
