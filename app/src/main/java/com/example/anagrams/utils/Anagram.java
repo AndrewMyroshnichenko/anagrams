34f3cbad3aa6aa21bb07e2dd7d8ec42940cde8e9
Andrew Miroshnichenko <mobilemiron@gmail.com>
2022-09-15 22:10:56 +0300

    Refactor: solved revese in 1 character array

--- app/src/main/java/com/example/anagrams/utils/Anagram.java
+++ app/src/main/java/com/example/anagrams/utils/Anagram.java
@@ -1,97 +1,51 @@
 package com.example.anagrams.utils;
 
-import java.util.Arrays;
-
 public class Anagram {
 
     public static String reverse(String inputString, String exceptChars){
-        final String [] tempArray = inputString.split("\\s");
+        final String [] words = inputString.split("\\s");
         StringBuilder reversedString = new StringBuilder();
 
-        for(int i = 0; i < tempArray.length; i++){
-            reversedString.append(reverseWord(tempArray[i], exceptChars)).append(" ");
+        for (String word : words) {
+            reversedString.append(reverseWord(word, exceptChars)).append(" ");
         }
             return  reversedString.toString().trim();
     }
 
-    private static char [] fillWithoutSelectedLetters(char [] inputArray, String exceptLetters){
-        char [] withoutSelectedLettersArray = new char[inputArray.length];
+    private static String reverseWord (String word, String filter){
+        char [] wordToChars = word.toCharArray();
 
-        if(!exceptLetters.equals("")) {
-            for (int i = 0; i < inputArray.length; i++) {
-                if(exceptLetters.indexOf(inputArray[i]) != -1){
-                    withoutSelectedLettersArray[i] = '\u0000';
-                } else {
-                    withoutSelectedLettersArray[i] = inputArray[i];
-                }
-            }
-        } else {
-            for (int i = 0; i < inputArray.length; i++) {
-                if (!Character.isLetter(inputArray[i])) {
-                    withoutSelectedLettersArray[i] = '\u0000';
-                } else {
-                    withoutSelectedLettersArray[i] = inputArray[i];
-                }
-            }
-        }
-        return withoutSelectedLettersArray;
-    }
-
-    private static char [] fillWithSelectedLetters(char [] inputArray, String exceptLetters){
-        char [] outputArray = new char[inputArray.length];
-
-        if(!exceptLetters.equals("")) {
-            for (int i = 0; i < inputArray.length; i++){
-                if(exceptLetters.indexOf(inputArray[i]) != -1){
-                    outputArray[i] = inputArray[i];
-                } else {
-                    outputArray[i] = '\u0000';
-                }
-            }
-        } else {
-            for (int i = 0; i < inputArray.length; i++) {
-                if (!Character.isLetter(inputArray[i])) {
-                    outputArray[i] = inputArray[i];
-                } else {
-                    outputArray[i] = '\u0000';
-                }
-            }
-        }
-        return outputArray;
-    }
-
-    private static void finalFillingOutputArray(char [] nonUsersLettersArray, char [] outputArray){
-        for (int i = 0; i < outputArray.length; i++) {
-            if(nonUsersLettersArray[i] != '\u0000') {
-                int counter = 0;
-                boolean charIsSet = false;
-                while (!charIsSet){
-                    if (outputArray[outputArray.length - 1 - counter] == '\u0000'){
-                        outputArray[outputArray.length - 1 - counter] = nonUsersLettersArray[i];
-                        charIsSet = true;
-                    } else {
-                        counter++;
+        if(!filter.equals("")) { // if filter is not empty
+            for (int i = 0; i < wordToChars.length / 2; i++) {
+                if(filter.indexOf(wordToChars[i]) == -1) { //if symbol from the first part is not included in the filter list
+                    for(int j = 0; j < wordToChars.length / 2; j++){
+                        if(filter.indexOf(wordToChars[wordToChars.length - j - 1]) == -1) { //if symbol from the second part is not included in the filter list
+                            char temp = wordToChars[wordToChars.length - i - 1];
+                            wordToChars[wordToChars.length - i - 1] = wordToChars[i];
+                            wordToChars[i] = temp;
+                            break;  // then swap symbols
+                        }
                     }
 
                 }
             }
+        } else {
+            for (int i = 0; i < wordToChars.length / 2; i++) {
+                if(Character.isLetter(wordToChars[i])){
+                    for(int j = 0; j < wordToChars.length; j++){
+                        if(Character.isLetter(wordToChars[wordToChars.length - j - 1])) {
+                            char temp = wordToChars[wordToChars.length - i - 1];
+                            wordToChars[wordToChars.length - i - 1] = wordToChars[i];
+                            wordToChars[i] = temp;
+                            break;
+                        }
+                    }
+                }
+            }
         }
+
+        return new String(wordToChars);
     }
-
-    private static String reverseWord (String word, String filter){
-        char [] inputWord = word.toCharArray();
-        char [] arrayWithoutUsersLetters = fillWithoutSelectedLetters(inputWord, filter);
-        char [] outputWord = new char[inputWord.length];
-        Arrays.fill(outputWord, (char) '\u0000');
-        outputWord = fillWithSelectedLetters(inputWord, filter);
-        finalFillingOutputArray(arrayWithoutUsersLetters, outputWord);
-
-        return new String(outputWord);
-    }
-
-
-
-
 }
 
 
