77fc71adfb63e2840fbfa532db374ca2ea62ac65
Andrew Miroshnichenko <mobilemiron@gmail.com>
2022-09-26 08:05:18 +0300

    Test: added new cases to the instrumental test

--- app/src/androidTest/java/com/example/anagrams/AnagramInstrumentalTest.java
+++ app/src/androidTest/java/com/example/anagrams/AnagramInstrumentalTest.java
@@ -23,18 +23,18 @@ public class AnagramInstrumentalTest {
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void basicTest() {
-        onView(withId(R.id.editText_input)).perform(ViewActions.typeText("Hello world!"));
-        onView(withId(R.id.textView_result)).check(matches(withText("olleH dlrow!")));
-    }
-
-    @Test
     public void withFilterTest(){
-        onView(withId(R.id.editText_filter)).perform(ViewActions.typeText(("XLxl")));
-        onView(withId(R.id.editText_input)).perform(ViewActions.typeText("Foxminded cool 24/7"));
-        onView(withId(R.id.textView_result)).check(matches(withText("dexdnimoF oocl 7/42")));
+        String [] filter = {"","","","","XxLl","XxLl","XxLl","XxLl","H"};
+        String [] input = {"He1llo World!", "Foxminded cool 24/7", "abcd efgh", "a1bcd efg!h","I wear XL t-shirt", "Foxminded cool 24/7", "abcd efgh", "dcb1a hgfle", "He1llo World!"};
+        String[] expectedResult = {"ol1leH dlroW!","dednimxoF looc 24/7","dcba hgfe","d1cba hgf!e","I raew XL trihs-t","dexdnimoF oocl 7/42","dcba hgfe","a1bcd efglh","Holl1e !dlroW"};
+        for (int i = 0; i < filter.length; ++i) {
+            onView(withId(R.id.editText_filter)).perform(ViewActions.typeText(filter[i]));
+            onView(withId(R.id.editText_input)).perform(ViewActions.typeText(input[i]));
+            onView(withId(R.id.textView_result)).check(matches(withText(expectedResult[i])));
+            onView(withId(R.id.editText_filter)).perform(ViewActions.clearText());
+            onView(withId(R.id.editText_input)).perform(ViewActions.clearText());
+        }
     }
-
 }
 
 
